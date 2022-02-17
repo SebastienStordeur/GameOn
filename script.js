40c77a7f69003588df6eeabe3782bb2afe72d84a
Sébastien Stordeur <stordeursebastien0@gmail.com>
2022-02-17 10:28:52 +0100

    test tourney counts, email

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ const errorMsg = {
   conditions: "Vous devez acceptez les conditions d'utilisation pour continuer"
 };
 const form = document.querySelector(".formData");
-/* const inputs = document.querySelectorAll(".text-control"); */
 const checkedBoxes = document.querySelectorAll(".checkbox-input");
-const inputs = document.querySelectorAll("input[type=text]")
-
+const inputs = document.querySelectorAll("input[type=text]");
+const email = document.querySelector("input[type=email]");
+const date = document.querySelector("input[type=date]");
+const tournament = document.querySelector("#quantity");
+const radios = document.querySelectorAll("input[type=radio]")
 
 
 //Regex
   //Lettres + quelques caractères spéciaux, accents et tirets + longueur de 2 
 const letterRegex = /^[\w'\-,.][^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{1,}$/;
 const emailRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+const integerRegex = /^\d+$/;
 
 //Close modal function
 function closeModal() {
@@ -52,11 +55,41 @@ function formCheck() {
   inputs.forEach((input) => {
     input.addEventListener("change", (e) => {
       if(letterRegex.test(input.value)) removeError(e.target)
-      else {
-        setError(e.target, errorMsg.name)
-      }
+      else setError(e.target, errorMsg.name)
     })
   })
+
+  function testEmail(){
+    email.addEventListener("change", (e) => {
+      if(emailRegex.test(email.value)) removeError(e.target)
+      else setError(e.target, errorMsg.email);
+    })
+  }
+
+  function testBirthdate() {
+    let birthDate = new Date(date.value)
+  }
+
+  function testRadios() {
+    radios.addEventListener("change", (e) => {
+      for(let radio of radios) {
+        if(radio.checked) removeError(e.target)
+      }
+      setError(radios, errorMsg.cities)
+    })
+  }
+
+  function tournamentCount() {
+    tournament.addEventListener("change", (e) => {
+      if(integerRegex.test(tournament.value)) removeError(e.target)
+      else setError(e.target, errorMsg.tournament);
+    });
+  };
+
+  testEmail()
+  testBirthdate()
+  tournamentCount()
+  testRadios()
 };
 
 formCheck()
